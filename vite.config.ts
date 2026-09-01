c07a99d764c6ea03d404db54102424a36b1bfabc
genesisazor <thegenesis3800@gmail.com>
2026-09-01 17:00:27 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: "/blue-sky-bumpers/",
+        base: "/wavy-studios-site/",
   server: {
     host: "::",
     port: 8080,
