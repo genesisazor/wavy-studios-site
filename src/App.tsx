d96090da001ea86151cb385cbbde5e99a9c1b759
genesisazor <thegenesis3800@gmail.com>
2026-09-01 17:03:44 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/blue-sky-bumpers">
+      <BrowserRouter basename="/wavy-studios-site">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/work" element={<Work />} />
